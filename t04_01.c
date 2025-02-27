2beb7cd67e1b705da8617a48c54e976baa728599
AdithyaSilaban <adithyasilaban@gmail.com>
2025-02-27 14:41:12 +0700

    update

--- t04_01.c
+++ t04_01.c
@@ -7,36 +7,25 @@
 
 int main(int _argv, char **_argc) {
 
-  char input[61]; 
-    fgets(input, sizeof(input), stdin); 
-    
-    
-    size_t length = strlen(input);
-    if (input[length - 1] == '\n') {
-        input[length - 1] = '\0';
-        length--;
+  char input[60];  
+  scanf("%s", input);
+
+  int length = strlen(input);
+  int jumlah_blocks = length / 3;
+  char blocks[jumlah_blocks][4];
+  char hasil[jumlah_blocks];
+
+  int i,j,k = 0;
+  for (i = 0; i < jumlah_blocks; i++) {
+    for (j = 0; j < 3; j++){
+      blocks[i][j] = input [k++];
+
     }
+    blocks[i][3] = '\0';
+    hasil[i] = (char)atoi(blocks[i]);
+    printf("%c", hasil[i]);
 
-    
-    if (length % 3 != 0) {
-        
-        return 1;
-    }
-
-    int jumlah_blocks = length / 3;
-    char hasil[jumlah_blocks + 1]; 
-    int k = 0;
-
-    for (int i = 0; i < jumlah_blocks; i++) {
-        char blok[4]; 
-        strncpy(blok, &input[k], 3);
-        blok[3] = '\0'; 
-        hasil[i] = (char)atoi(blok); 
-        k += 3;
-    }
-
-    hasil[jumlah_blocks] = '\0'; 
-    printf("%s\n", hasil); 
-
-    return 0;
+  }
+  return 0;
 }
+  
