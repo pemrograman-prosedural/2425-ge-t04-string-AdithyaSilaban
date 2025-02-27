4fd59500479b94de837bd6621faf16a17f39da33
AdithyaSilaban <adithyasilaban@gmail.com>
2025-02-27 14:26:18 +0700

    Kami telah menyelesaikan tugas (Pair 10)

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,31 @@
-// NIM - Name
-// NIM - Name
+// 12S24029 - Adithya Silaban
+// 12S24009 - Kezia Pasaribu
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
 
+  char input[60];  
+  scanf("%s", input);
+
+  int length = strlen(input);
+  int jumlah_blocks = length / 3;
+  char blocks[jumlah_blocks][4];
+  char hasil[jumlah_blocks];
+
+  int i,j,k = 0;
+  for (i = 0; i < jumlah_blocks; i++) {
+    for (j = 0; j < 3; j++){
+      blocks[i][j] = input [k++];
+
+    }
+    blocks[i][3] = '\0';
+    hasil[i] = (char)atoi(blocks[i]);
+    printf("%c", hasil[i]);
+
+  }
   return 0;
 }
+  
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,23 @@
-// NIM - Name
-// NIM - Name
+// 12S24029 - Adithya Silaban
+// 12S24009 - Kezia Pasaribu
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+
+  char input[20];
+  fgets(input, sizeof(input), stdin);
+
+  int length = strlen(input);
+  if (length > 0 && input[length - 1] == '\n') {
+    input[length - 1] = '\0';
+  }
+    for (int i = 0; i < length - 1; i++) {
+      printf("%03d", input[i]);
+    }
+  
+
 
   return 0;
 }
